4562c20b5df88c0e240e25aa890a436af01a8bef
kamila <dosymbekkamila@gmail.com>
2025-04-27 14:56:48 +0500

    second commit

---
+++ app/src/main/java/com/example/lab9/activities/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.lab9.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.spotifyclone.R;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
