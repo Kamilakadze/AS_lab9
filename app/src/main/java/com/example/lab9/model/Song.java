c50b585ec49553047bb30085e1832aae7d81c09c
kamila <dosymbekkamila@gmail.com>
2025-04-27 15:17:44 +0500

    commit 4

---
+++ app/src/main/java/com/example/lab9/model/Song.java
@@ -0,0 +1,38 @@
+package com.example.lab9.model;
+
+public class Song {
+    private String title;
+    private String artist;
+    private String filePath;
+
+    public Song(String title, String artist, String filePath) {
+        this.title = title;
+        this.artist = artist;
+        this.filePath = filePath;
+    }
+
+    // Геттеры и сеттеры
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
