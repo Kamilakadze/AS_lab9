0bab8a66e31c4eb8497732f0680edbf5f54959be
kamila <dosymbekkamila@gmail.com>
2025-04-27 15:04:45 +0500

    third commit

--- app/src/main/java/com/example/lab9/activities/HomeActivity.java
+++ app/src/main/java/com/example/lab9/activities/HomeActivity.java
@@ -1,13 +1,32 @@
 package com.example.lab9.activities;
 
+import com.example.lab9.R;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.spotifyclone.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.menu_home:
+                    return true; // Уже на Home
+                case R.id.menu_search:
+                    startActivity(new Intent(this, SearchActivity.class));
+                    return true;
+                case R.id.menu_library:
+                    startActivity(new Intent(this, PlaylistActivity.class));
+                    return true;
+            }
+            return false;
+        });
     }
 }
---
+++ app/src/main/java/com/example/lab9/activities/SearchActivity.java
@@ -0,0 +1,13 @@
+package com.example.lab9.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.lab9.R;
+
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
